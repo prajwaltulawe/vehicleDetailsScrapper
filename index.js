8c7c74aef1868321bf40201eea048030e19e3dc2
Prajwal Tulawe <tulaveprajwal143@gmail.com>
2022-09-09 18:38:24 +0530

    v2

--- index.js
+++ index.js
@@ -41,10 +41,20 @@ app.get('/vehicleNo', (req, res) =>{
                                 <h1> Enter Captcha.. </h1>
                                 <img src="../captchaImages/${imgPath}.jpg" alt="" srcset="">
                                 <input type="text" name="captcha">
-                                <button type="submit"> Submit</button>
+                                <button type="submit" onclick="showDetails()"> Submit</button>
                             </form>
                         </body>
+                        <script>
+                            function openWindow(){
+                                window.open("http://localhost:8000/details.html"); 
+                                clearInterval(openPage);
+                            }
+                            function showDetails(){
+                                const openPage = setInterval(openWindow, 10000);
+                            }
+                        </script>
                     </html>`);
+        return 0;
     } 
 
     if (req.query.vehicleNum) {
@@ -87,44 +97,78 @@ app.get('/vehicleNo', (req, res) =>{
                     }
                 });
         
-            getCaptcha(captchaImageName);
+            await getCaptcha(captchaImageName);
 
             app.get('/getCaptcha', async (req, res) =>{
                 var captchaInput = await page.waitForXPath("//input[@id='vahancaptcha:CaptchaID']");
                 await captchaInput.type(req.query.captcha);
-            });
+                await page.click('#j_idt55'); 
+                await new Promise(r => setTimeout(r, 3000));        
+                var data =  await page.evaluate(() => {
+                    const details = {};
+                    details.vehicleNo = document.querySelector('[title="Registration Number"]').innerText;
+                    details.status = document.querySelector('[title="RC Status"]').innerText.substring(7,22);
+                    details.class = document.querySelector('[title="Vehicle Class"]').innerText;
+                    details.fuel = document.querySelector('[title="Fuel"]').innerText;
+                    details.emissionNorms = document.querySelector('[title="Emission norms"]').innerText;
+                    details.modelName = document.querySelector('[title="Model Name"]').innerText;
+                    details.manufacturerName = document.querySelector('[title="Manufacturer Name"]').innerText;
+                    details.registeringAuthority = document.querySelector('[title="Registering Authority"]').innerText;
+                    details.ownerName = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[0].innerText;
+                    details.registrationDate = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[1].innerText;
+                    details.fitnessRegn  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[2].innerText;
+                    details.mvTax  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[3].innerText;
+                    details.insurance  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[4].innerText;
+                    details.pucc  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[4].children[0].innerText;
+                    return  details;
+                });
 
-            await page.click('#j_idt58'); 
-            var data =  await page.evaluate(() => {
- 
-                const details = {};
-                details.vehicleNo = document.querySelector('[title="Registration Number"]').innerText;
-                details.status = document.querySelector('[title="RC Status"]').innerText.substring(7,22);
-                details.class = document.querySelector('[title="Vehicle Class"]').innerText;
-                details.fuel = document.querySelector('[title="Fuel"]').innerText;
-                details.emissionNorms = document.querySelector('[title="Emission norms"]').innerText;
-                details.modelName = document.querySelector('[title="Model Name"]').innerText;
-                details.manufacturerName = document.querySelector('[title="Manufacturer Name"]').innerText;
-                details.registeringAuthority = document.querySelector('[title="Registering Authority"]').innerText;
-                details.ownerName = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[0].innerText;;
-                details.registrationDate = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[1].innerText;;
-                details.fitnessRegn  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[2].innerText;;
-                details.MvTax  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[3].innerText;;
-                details.insurance  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[4].innerText;;
-                details.pucc  = document.getElementsByClassName('col-md-3 fit-width-content font-bold content-resize')[5].innerText;;
-                return  details;
-            }); 
-        
-            await new Promise(r => setTimeout(r, 1000));
-            
-            fs.writeFile('data.txt', data, err =>{
-                if (err) {
-                    console.err;
-                }
-            }); 
-        
+                var dataHtml = `<!DOCTYPE html>
+                                <html lang="en">
+                                    <head>
+                                        <meta charset="UTF-8" />
+                                        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />    
+                                        <style>
+                                            body {
+                                            width: 80%;
+                                            margin: auto;
+                                            margin-top: 5%;
+                                            font-family: "Courier New", Courier, monospace;
+                                            }                                          
+                                            tr:nth-child(even){background-color: #f2f2f2;}
+                                            tr:hover {background-color: #ddd;}
+                                        </style>
+                                        <title>Vehicle Details</title>
+                                    </head>
+                                    <body>
+                                        <table id="table_id" class="display">
+                                            <tr> <td>Vehicle No</td> <td> ${data.vehicleNo} </td> </tr>
+                                            <tr> <td>Stauts</td> <td> ${data.status} </td> </tr>
+                                            <tr> <td>Class</td> <td> ${data.class} </td> </tr>
+                                            <tr> <td>Fuel</td> <td> ${data.fuel} </td> </tr>
+                                            <tr> <td>Emmission Norm</td> <td> ${data.emissionNorms}} </td> </tr>
+                                            <tr> <td>Model Name</td> <td> ${data.modelName} </td> </tr>
+                                            <tr> <td>Manufacturer Name</td> <td> ${data.manufacturerName} </td> </tr>
+                                            <tr> <td>Registring Authority</td> <td> ${data.registeringAuthority} </td> </tr>
+                                            <tr> <td>Owner Name</td> <td> ${data.ownerName} </td> </tr>
+                                            <tr> <td>Registration Date</td> <td> ${data.registrationDate} </td> </tr>
+                                            <tr> <td>Fitness Regn</td> <td> ${data.fitnessRegn} </td> </tr>
+                                            <tr> <td>MV Tax</td> <td> ${data.mvTax} </td> </tr>
+                                            <tr> <td>Insurance</td> <td> ${data.insurance} </td> </tr>
+                                            <tr> <td>Pucc</td> <td> ${data.pucc} </td> </tr>
+                                        </table>
+                                    </body>
+                                </html>`;
+                fs.writeFile('./public/details.html', dataHtml, err =>{
+                    if (err) {
+                        console.err;
+                    }
+                });           
+            });
+            // await browser.close();
         })();
-        // res.end(getCaptcha);
+        app.use(express.static('public/details.html'));
     }else{
         res.send(`<center><h1>Some error occoured... </h1></center>`);
     }
